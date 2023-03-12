27e8d946de3f6ccbc018978740ba5e4e387b2b9e
Pronicio <52421988+Pronicio@users.noreply.github.com>
2023-03-12 21:57:35 +0100

    🎉 Initialisation.

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <div class="brand">
+      <div class="logo"></div>
+      <h3>Vexa Studio</h3>
+    </div>
+    <nav>
+      <a href="#" id="active">Accueil</a>
+      <a href="#">Graphistes</a>
+      <a href="#">Portfolio</a>
+      <button>Contact</button>
+    </nav>
+  </header>
+  <main>
+    <section id="home">
+      <div class="title">
+        <button>Fièrement québécois!</button>
+        <h1>Nous Concevons Votre<br><b>Indentité Graphique</b></h1>
+        <p>Vexa Studio est un studio de conception graphique<br>
+          visant à créer et développer l'image visuelle projetée par nos<br>
+          clients partout sur le web.</p>
+      </div>
+      <img src="../assets/images/palette.png" alt="Palette">
+    </section>
+  </main>
+</template>
+
+<style lang="scss">
+@import "assets/style/style.scss";
+</style>
+
+<style scoped lang="scss">
+@import "assets/style/pages/home.scss";
+</style>
