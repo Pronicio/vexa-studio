3ba7f930935407bd3a9e4c3c13987ca2e504d602
Pronicio <52421988+Pronicio@users.noreply.github.com>
2023-04-25 21:58:18 +0200

    ✨ New : why, reviews, stats, creations and footer sections.

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,170 @@
 <template>
-  <header>
-    <div class="brand">
-      <div class="logo"></div>
-      <h3>Vexa Studio</h3>
+    <header>
+        <div class="brand">
+            <div class="logo"></div>
+            <h3>Vexa Studio</h3>
+        </div>
+        <nav>
+            <a href="#" id="active">Accueil</a>
+            <a href="#">Équipe</a>
+            <a href="#">Services</a>
+            <button>Contact</button>
+        </nav>
+    </header>
+    <main>
+        <section id="home">
+            <div class="title">
+                <button>Fièrement québécois!</button>
+                <h1>Nous Concevons Votre<br><b>Indentité Graphique</b></h1>
+                <p>Vexa Studio est un studio de conception graphique<br>
+                    visant à créer et développer l'image visuelle projetée par nos<br>
+                    clients partout sur le web.</p>
+            </div>
+            <img src="../assets/images/palette.png" alt="Palette">
+        </section>
+        <section id="why">
+            <div class="title">
+                <h2>Nous <b>sommes idéals</b></h2>
+                <p>Voici pourquoi nous sommes un bon choix pour la<br>
+                    création de vos différents contenus graphiques.
+                </p>
+            </div>
+            <div class="collection">
+                <div class="card">
+                    <img src="../assets/images/graphics/discord.png" alt="Discord">
+                    <h4>Commande via Discord</h4>
+                    <p>Grâce à notre bot, il est possible de<br>
+                        commander à l'aide d'un procédé<br>
+                        rapide et interactif.<br>
+                    </p>
+                </div>
+                <div class="card">
+                    <img src="../assets/images/graphics/money.png" alt="Money">
+                    <h4>Prix peu coûteux</h4>
+                    <p>Nous rendons le prix des commandes<br>
+                        le plus bas possible pour vous permettre<br>
+                        de ne pas vous ruiner lors du paiement.<br>
+                    </p>
+                </div>
+                <div class="card">
+                    <img src="../assets/images/graphics/file.png" alt="File">
+                    <h4>Accès aux fichiers finaux</h4>
+                    <p>Nous vous donnons l'accès aux fichiers<br>
+                        une fois le projet approuvé pour vous<br>
+                        permettre de le modifier plus tard.<br>
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section id="reviews">
+            <div class="title">
+                <h2>Nous <b>sommes idéals</b></h2>
+                <p>Voici pourquoi nous sommes un bon choix pour la<br>
+                    création de vos différents contenus graphiques.
+                </p>
+            </div>
+            <div class="collection">
+                <div class="review">
+                    <img src="../assets/images/user-avatar.png" alt="User avatar">
+                    <h5>Lorem Ipsum</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur<br>
+                        adipiscing elit, sed do eiusmod tempor<br>
+                        incididunt ut labore et dolore magna aliqua.<br>
+                    </p>
+                </div>
+                <div class="review">
+                    <img src="../assets/images/user-avatar.png" alt="User avatar">
+                    <h5>Lorem Ipsum</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur<br>
+                        adipiscing elit, sed do eiusmod tempor<br>
+                        incididunt ut labore et dolore magna aliqua.<br>
+                    </p>
+                </div>
+                <div class="review">
+                    <img src="../assets/images/user-avatar.png" alt="User avatar">
+                    <h5>Lorem Ipsum</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur<br>
+                        adipiscing elit, sed do eiusmod tempor<br>
+                        incididunt ut labore et dolore magna aliqua.<br>
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section id="stats">
+            <div class="title">
+                <h2>Quelques <b>statistiques</b></h2>
+                <p>Voici à quoi ressemble Vexa Studio en chiffres.</p>
+            </div>
+            <div class="collection">
+                <div class="card">
+                    <h5>20+</h5>
+                    <p>Commandes<br>
+                        Passées
+                    </p>
+                </div>
+                <div class="card">
+                    <h5>30+</h5>
+                    <p>Devis<br>
+                        Donnés
+                    </p>
+                </div>
+                <div class="card">
+                    <h5>5+</h5>
+                    <p>Membres<br>
+                        Du staff
+                    </p>
+                </div>
+                <div class="card">
+                    <h5>100+</h5>
+                    <p>Membres<br>
+                        Sur Discord
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section id="creations">
+            <div class="title">
+                <h2>Nos <b>dernières créations</b></h2>
+                <p>Voici les dernières commandes que nous avons<br>
+                    effectuées pour nos clients.
+                </p>
+            </div>
+            <div class="collection">
+                <div class="card">
+                    <img src="../assets/images/us/banner_tom.png" alt="Tom's banner" width="509">
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer>
+        <div class="column">
+            <div class="brand">
+                <div class="logo"></div>
+                <h3>Vexa Studio</h3>
+            </div>
+            <p>
+                Vexa Studio est un studio de conception<br>
+                graphique visant à créer et développer<br>
+                l'image visuelle projetée par nos clients<br>
+                partout sur le web.
+            </p>
+        </div>
+        <div class="column">
+            <h5>Vexa Studio</h5>
+            <a href="#">Accueil</a>
+            <a href="#">Équipe</a>
+            <a href="#">Services</a>
+        </div>
+        <div class="column">
+            <h5>Réseaux Sociaux</h5>
+            <a href="#">Discord</a>
+            <a href="#">Twitter</a>
+            <a href="#">Behance</a>
+        </div>
+    </footer>
+    <div class="copyright">
+        <p>©️ Vexa Studio 2023. Toute reproduction est interdite.</p>
     </div>
-    <nav>
-      <a href="#" id="active">Accueil</a>
-      <a href="#">Graphistes</a>
-      <a href="#">Portfolio</a>
-      <button>Contact</button>
-    </nav>
-  </header>
-  <main>
-    <section id="home">
-      <div class="title">
-        <button>Fièrement québécois!</button>
-        <h1>Nous Concevons Votre<br><b>Indentité Graphique</b></h1>
-        <p>Vexa Studio est un studio de conception graphique<br>
-          visant à créer et développer l'image visuelle projetée par nos<br>
-          clients partout sur le web.</p>
-      </div>
-      <img src="../assets/images/palette.png" alt="Palette">
-    </section>
-  </main>
 </template>
 
 <style lang="scss">
@@ -32,3 +174,5 @@
 <style scoped lang="scss">
 @import "assets/style/pages/home.scss";
 </style>
+<script setup lang="ts">
+</script>
