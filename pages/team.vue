eaf8bf7f3583a31af362fd3bf695fa17ddb29875
Pronicio <52421988+Pronicio@users.noreply.github.com>
2023-04-27 18:17:25 +0200

    ✨ Team page.

--- pages/team.vue
+++ pages/team.vue
@@ -1,5 +1,88 @@
 <template>
-
+    <main>
+        <div class="title">
+            <h1>Notre <b>super équipe</b></h1>
+            <p>Vexa Studio possède une équipe dévouée, sérieuse<br>
+                et profesionnelle qui a pour but votre satisfaction.</p>
+        </div>
+        <section>
+            <h3>Pôle Gestion</h3>
+            <div class="collection">
+                <div class="card">
+                    <img src="https://i.imgur.com/8AaJjoo.jpeg" alt="Avatar" width="220">
+                    <div class="user">
+                        <h4>Artix</h4>
+                        <p>Lorem Ipsum</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://i.imgur.com/8AaJjoo.jpeg" alt="Avatar" width="220">
+                    <div class="user">
+                        <h4>Tom</h4>
+                        <p>Lorem Ipsum</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <h3>Pôle Artistique</h3>
+            <div class="collection">
+                <div class="card">
+                    <img src="https://i.imgur.com/8AaJjoo.jpeg" alt="Avatar" width="220">
+                    <div class="user">
+                        <h4>Alex</h4>
+                        <p>Lorem Ipsum</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://i.imgur.com/8AaJjoo.jpeg" alt="Avatar" width="220">
+                    <div class="user">
+                        <h4>Superangers</h4>
+                        <p>Lorem Ipsum</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://i.imgur.com/8AaJjoo.jpeg" alt="Avatar" width="220">
+                    <div class="user">
+                        <h4>Pronicio</h4>
+                        <p>Lorem Ipsum</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <h3>Pôle Clientèle</h3>
+            <div class="collection">
+                <div class="card">
+                    <img src="https://i.imgur.com/8AaJjoo.jpeg" alt="Avatar" width="220">
+                    <div class="user">
+                        <h4>Luciferdawns</h4>
+                        <p>Lorem Ipsum</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://i.imgur.com/8AaJjoo.jpeg" alt="Avatar" width="220">
+                    <div class="user">
+                        <h4>Elroc</h4>
+                        <p>Lorem Ipsum</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="https://i.imgur.com/8AaJjoo.jpeg" alt="Avatar" width="220">
+                    <div class="user">
+                        <h4>Pronicio</h4>
+                        <p>Lorem Ipsum</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="application">
+            <h2>Notre <b>super équipe</b></h2>
+            <p>Vexa Studio possède une équipe dévouée, sérieuse<br>
+                et profesionnelle qui a pour but votre satisfaction.</p>
+            <button>Nous Joindre</button>
+        </div>
+    </main>
 </template>
 
 <style scoped lang="scss">
