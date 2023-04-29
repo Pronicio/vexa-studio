9f1c8bd44a640f56874730ff0fe693a69623c4ab
Pronicio <52421988+Pronicio@users.noreply.github.com>
2023-04-29 21:42:11 +0200

    ✨ Services page.

---
+++ pages/services.vue
@@ -0,0 +1,51 @@
+<template>
+  <main>
+      <div class="title reveal fade-left">
+          <h1>Nos <b>différents services</b></h1>
+          <p>Chez Vexa Studio, nous offrons une gamme de services<br>
+              attrayante pour leur excellent rapport qualité-prix.</p>
+      </div>
+      <div class="collection">
+          <div class="card">
+              <img src="../assets/images/service.png" alt="Service image" width="770" class="reveal fade-right">
+              <div class="reveal fade-left">
+                  <h3>Design Graphique</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur<br>
+                      adipiscing elit, sed do eiusmod tempor.</p>
+                  <button>Commander</button>
+              </div>
+          </div>
+          <div class="card">
+              <img src="../assets/images/service.png" alt="Service image" width="770" class="reveal fade-right">
+              <div class="reveal fade-left">
+                  <h3>Modelage 3D</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur<br>
+                      adipiscing elit, sed do eiusmod tempor.</p>
+                  <button>Commander</button>
+              </div>
+          </div>
+          <div class="card">
+              <img src="../assets/images/service.png" alt="Service image" width="770" class="reveal fade-right">
+              <div class="reveal fade-left">
+                  <h3>Motion Design</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur<br>
+                      adipiscing elit, sed do eiusmod tempor.</p>
+                  <button>Commander</button>
+              </div>
+          </div>
+          <div class="card">
+              <img src="../assets/images/service.png" alt="Service image" width="770" class="reveal fade-right">
+              <div class="reveal fade-left">
+                  <h3>Développement Web</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur<br>
+                      adipiscing elit, sed do eiusmod tempor.</p>
+                  <button>Commander</button>
+              </div>
+          </div>
+      </div>
+  </main>
+</template>
+
+<style scoped lang="scss">
+@import "assets/style/pages/services.scss";
+</style>
